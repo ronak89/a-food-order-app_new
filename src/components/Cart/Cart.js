f17178f870619f2868d28697f1f0ec8340d47da3
Ronak Patel <patelronak1989@gmail.com>
2022-04-14 19:36:17 +0530

    a-food-order

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,27 +1,53 @@
-import classes from "./Cart.module.css";
-import Model from "../UI/Model";
+import { useContext } from 'react';
 
-const Cart = () => {
+import Modal from '../UI/Modal';
+import CartItem from './CartItem';
+import classes from './Cart.module.css';
+import CartContext from '../../store/cart-context';
+
+const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
+
+  const cartItemAddHandler = (item) => {
+    cartCtx.addItem({ ...item, amount: 1 });
+  };
+
   const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {[{ id: "C1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li>{item.name}</li>
+    <ul className={classes['cart-items']}>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
 
   return (
-    <Model>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
-        <spa>Total Amount</spa>
-        <spa>35.62</spa>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button--alt"]}>Close</button>
-        <button className={classes.button}>Order</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>
+          Close
+        </button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
-    </Model>
+    </Modal>
   );
 };
 
